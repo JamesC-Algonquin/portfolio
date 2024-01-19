9104a66be32936c9ea96d49ab9b51b2c368b527e
James <jarech948@gmail.com>
2024-01-19 12:40:45 -0400

    Style Updates

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import '../styles.css'
 export default function Home() {
   return (
     <>
-    <div className="front">
+    <div className="content">
         <h1>James Ching</h1>
         <h2>Software Developer</h2>
         <p>Hello, my name is James Ching. I am a software developer skilled in many languages and technologies, 
