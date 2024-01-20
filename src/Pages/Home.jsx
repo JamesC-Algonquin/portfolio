d7298e89cdce421d1ea7aeeff04c90f1333a7816
James <jarech948@gmail.com>
2024-01-20 00:02:59 -0400

    Home Page Content

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,9 +8,13 @@ export default function Home() {
     initial={{opacity:0}}
     animate={{opacity:1}}
     exit={{opacity:0}}>
+      <br/>
         <h1>James Ching</h1>
         <h2>Software Developer</h2>
-        
+        <div className='front'>
+        <p>Hello, my name is James Ching. I make various applications for desktop, web, and mobile development.
+          I strongly believe effective coding, logical structure, and good communication is what makes the best software.
+        </p></div>
       </motion.div>
     
   )
