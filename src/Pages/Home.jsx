f7e8689ed4572cd9514f6b1afa4ea69ebe719735
James <jarech948@gmail.com>
2024-01-31 17:41:37 -0400

    Home Page Wording

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
         <h2>Software Developer</h2>
         <div className='front'>
         <p>
-          I am a software developer with experience making various applications for desktop, web, and mobile environments. 
+          I am a software developer with experience making applications for desktop, web, and mobile environments. 
           Throughout my programming career, my focus has been on effective, 
           concise programming, logical structure and design, and good communication.
         </p>
